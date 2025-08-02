31f92a87785e5d2fa5a7a13bc20ee9e2df88e45d
ariansust21 <127542579+pillowarian@users.noreply.github.com>
2025-08-02 13:22:38 +0600

    Resolved all build warnings and confirmed asset paths

--- src/App.jsx
+++ src/App.jsx
@@ -61,7 +61,7 @@ const App = () => {
 
   useEffect(() => {
     const defaultCity = "Dhaka";
-   const API_URL = `http://api.weatherapi.com/v1/forecast.json?key=${API_KEY}&q=${defaultCity}&days=2`;
+   const API_URL = `https://api.weatherapi.com/v1/forecast.json?key=${API_KEY}&q=${defaultCity}&days=2`;
   getWeatherDetails(API_URL);
   },[]);
 
